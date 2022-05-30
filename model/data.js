51914c466db26cb8c61a1330ff4290f3e78a9830
nestaenow <nestaenow@gmail.com>
2022-05-30 21:00:11 +0100

    Image asynchronous data

---
+++ model/data.js
@@ -0,0 +1,122 @@
+export const sliderData = [
+    {
+        title: 'First Game',
+        image: require('../assets/books/SinEater.jpg')
+    },
+    {
+        title: 'Second Game',
+        image: require('../assets/books/WilderGirls.jpg')
+    },
+    {
+        title: 'Third Game',
+        image: require('../assets/books/HarryPotter.jpg')
+    },
+    {
+        title: 'Fourth Game',
+        image: require('../assets/books/TheWorst.jpg')
+    },
+]
+
+export const bestSeller = [
+    {
+        poster: require('../assets/books/SinEater.jpg'),
+        title: 'Sin Eater',
+        genre: 'Thriller',
+        price: '5000XAF',
+        id: '1',
+    },
+    {
+        image: require('../assets/books/SixOfCrows.jpg'),
+        title: 'Six Of Crows',
+        genre: 'Fiction',
+        price: '3000XAF',
+        id: '2',
+    },
+    {
+        image: require('../assets/books/TheWorst.jpg'),
+        title: 'The Worst',
+        genre: 'Thriller',
+        price: '5000XAF',
+        id: '3',
+    },
+    {
+        image: require('../assets/books/HarryPotter.jpg'),
+        title: 'Harry Potter',
+        genre: 'Adventure',
+        price: '8000XAF',
+        id: '4',
+    },
+    {
+        image: require('../assets/books/TheGirlInRed.jpg'),
+        title: 'The Girl In Red',
+        genre: 'Fantasy',
+        price: '6000XAF',
+        id: '5',
+    },
+    {
+        image: require('../assets/books/WilderGirls.jpg'),
+        title: 'Wilder Girls',
+        genre: 'Romance',
+        price: '8000XAF',
+        id: '6',
+    },
+]
+
+export const theLatest = [
+    {
+        image: require('../assets/books/AllThisTime.jpg'),
+        title: 'All This Time',
+        genre: 'Adventure',
+        price: '6000XAF',
+        id: '1',
+    },
+    {
+        image: require('../assets/books/TheKingOfDrugs.jpg'),
+        title: 'The King Of Drugs',
+        genre: 'Thriller',
+        price: '8000XAF',
+        id: '2',
+    },
+    {
+        image: require('../assets/books/IT.jpg'),
+        title: 'IT',
+        genre: 'Horror',
+        price: '10000XAF',
+        id: '3',
+    },
+    {
+        image: require('../assets/books/FiveFeetApart.jpg'),
+        title: 'Five Feet Apart',
+        genre: 'Romance',
+        price: '8000XAF',
+        id: '4',
+    },
+    {
+        image: require('../assets/books/SonOfTheStorm.jpg'),
+        title: 'Son Of The Storm',
+        genre: 'Adventure',
+        price: '5000XAF',
+        id: '5',
+    },
+    {
+        image: require('../assets/books/TheOutsider.jpg'),
+        title: 'The Outsider',
+        genre: 'Romance',
+        price: '8000XAF',
+        id: '6',
+    },
+    {
+        image: require('../assets/books/TessOfTheRoad.jpg'),
+        title: 'Tess Of The Road',
+        genre: 'Fantasy',
+        price: '9000XAF',
+        id: '7',
+    },
+    {
+        image: require('../assets/books/WilderGirls.jpg'),
+        title: 'Wilder Girls',
+        genre: 'Romance',
+        price: '8000XAF',
+        id: '8',
+    },
+]

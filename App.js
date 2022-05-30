aeb6053156d39299170bfeca945e2dc00df7759c
nestaenow <nestaenow@gmail.com>
2022-05-30 12:14:08 +0100

    progress

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -10,15 +10,18 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator style = {styles.tabNavigator} screenOptions={{ tabBarShowLabel: false }}>
-      <Tab.Screen name='Home' component={HomeScreen} screenOptions = {{ tabBarIcon: ({focused}) => (
-        <View>
-            <Image source={require('../assets/home.png')}/>
-            <Text style={{color: focused ? '#6B3F87' : '#CB78FF'}}>
-                Home
-            </Text>
-        </View>
-    )}}/>
+    <Tab.Navigator style = {styles.tabNavigator} screenOptions={{ tabBarShowLabel: false, headerShown: false }}>
+      <Tab.Screen name='Home' component={HomeScreen} screenOptions = {{ tabBarIcon: ({ focused }) => {
+        const image = focused
+        ? require('../assets/HomeActive.png')
+        : require('../assets/HomeInactive.png')
+        return (
+            <Image
+                source={image}
+                style={{height:20, width:20}}
+            />
+        )
+    }}}/>
       <Tab.Screen name='SerachBook' component={SearchBookScreen}/>
       <Tab.Screen name='Books' component={BooksScreen}/>
       <Tab.Screen name='Menu' component={MenuScreen}/>

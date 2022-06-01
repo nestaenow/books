1548033da8e9d15ead072aad981ab948c4a8bff9
nestaenow <nestaenow@gmail.com>
2022-06-01 12:42:07 +0100

    progress

--- App.js
+++ App.js
@@ -19,13 +19,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Splash Screen'>
-        <Stack.Screen options={{ headerShown: false }} name="Splash Screen" component={SplashScreen} />
+        {/* <Stack.Screen options={{ headerShown: false }} name="Splash Screen" component={SplashScreen} />
         <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
         <Stack.Screen options={{ headerShown: false }} name="SignUp" component={SignUpScreen} />
         <Stack.Screen options={{ headerShown: false }} name="ConfirmEmail" component={ConfirmEmailScreen} />
         <Stack.Screen options={{ headerShown: false }} name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen options={{ headerShown: false }} name="NewPassword" component={NewPasswordScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} /> 
+        <Stack.Screen options={{ headerShown: false }} name="Home" component={HomeScreen} />  */}
         <Stack.Screen options={{ headerShown: false }} name="BookDetails" component={BookDetailsScreen} /> 
         {/* <Stack.Screen options={{ headerShown: false }} name="Land" component={LandScreen} /> */}
       </Stack.Navigator>
--- screens/BookDetailsScreen.js
+++ screens/BookDetailsScreen.js
@@ -1,14 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, SafeAreaView } from 'react-native'
 import React from 'react'
+import { windowHeight } from '../utils/Dimensions'
 
-const BookDetails = () => {
+const BookDetailsScreen = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>BookDetails</Text>
-    </View>
+    // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.cover}>
+          <ImageBackground source={require('../assets/books/FiveFeetApart.jpg')} resizeMode="cover" style={styles.image}>
+            {/* <Image source={require('../assets/books/FiveFeetApart.jpg')} style={{width: 240 , height: 360}}/> */}
+            <View style={{opacity:1}}>
+              <Text style={styles.text}>yfgjvjgvkhvkhgvogvnfgnjfn</Text>
+            </View>
+            
+          </ImageBackground>
+        </View>
+        <View></View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-export default BookDetails
+export default BookDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  cover: {
+    // paddingVertical: 80,
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6B3F87',
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    width: '100%',
+    height: windowHeight * 0.64,
+    opacity: 0.3
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+  }
+})

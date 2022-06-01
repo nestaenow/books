c69cd48b4de86a181c4d1c3bced2b9a82320a35b
nestaenow <nestaenow@gmail.com>
2022-06-01 18:03:10 +0100

    progress

--- screens/BookDetailsScreen.js
+++ screens/BookDetailsScreen.js
@@ -9,13 +9,21 @@ const BookDetailsScreen = () => {
       <ScrollView>
         <View style={styles.cover}>
           <ImageBackground source={require('../assets/books/FiveFeetApart.jpg')} resizeMode="cover" blurRadius={15} style={styles.image}>
-            <Image source={require('../assets/books/FiveFeetApart.jpg')} style={{width: 240 , height: 360}}/>
+            <Image source={require('../assets/books/FiveFeetApart.jpg')} style={{width: 200 , height: 300}}/>
+            <View style={{marginVertical: 20, alignItems: 'center'}}>
+              <Text style={{color: '#FFF', fontSize: 22, fontWeight: '700', marginBottom: 4}}>Five Feet Apart</Text>
+              <Text style={{color: '#FFF', fontWeight: '300'}}>Mikki Daughtry && Tobias Laconis</Text>
+            </View>
             <View style={styles.info}>
-              <View>
+              <View style={styles.infos}>
+                <Text style={styles.text1}>ENG</Text>
+                <Text style={styles.text2}>Language</Text>
+              </View>
+              <View style={[styles.infos, {borderRightWidth: 0.3, borderLeftWidth: 0.3, borderColor: '#FFF', paddingHorizontal: 20}]}>
                 <Text style={styles.text1}>190</Text>
                 <Text style={styles.text2}>Number of Pages</Text>
               </View>
-              <View>
+              <View style={styles.infos}>
                 <Text style={styles.text1}>Fantasy</Text>
                 <Text style={styles.text2}>Genre</Text>
               </View>
@@ -44,21 +52,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: 'center',
     width: '100%',
+    // paddingTop: 50,
     height: windowHeight * 0.64,
   },
   info: {
-    marginVertical: 20,
     backgroundColor: '#6B3F87',
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    paddingHorizontal: 80,
+    width: '80%',
     paddingVertical: 25,
     borderRadius: 10,
+  },
+  infos: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text1: {
     color: '#fff',
     fontWeight: '700',
+    fontSize: 15,
   },
   text2: {
     color: '#fff',
